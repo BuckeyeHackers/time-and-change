1275d487ec1c3222477631d74fd36e98d37e744a
Jackson Killian <jakillian.34@gmail.com>
2017-01-22 02:17:13 -0500

    javascript working on popup

---
+++ basic.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('lbody');
+    link.innerHTML = "KEVIN"
+
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+    });
+});
